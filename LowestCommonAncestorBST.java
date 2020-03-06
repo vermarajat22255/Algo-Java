7f90f1a1a390215cf03d5ddd0be0d44eff24f1ec
Rajat verma <45594149+vermarajat22255@users.noreply.github.com>
2020-03-05 21:37:14 -0500

    Create LowestCommonAncestorBST.java

---
+++ LowestCommonAncestorBST.java
@@ -0,0 +1,13 @@
+public static Node lca(Node root, int v1, int v2) {
+      	// Write your code here.
+        if(root == null) return null;
+        if(root.data == v1 || root.data == v2) return root;
+
+        Node left = lca(root.left, v1, v2);
+        Node right = lca(root.right, v1, v2);
+
+        if(left != null && right != null) return root;
+        if(left == null && right == null) return null;
+
+        return left!=null?left:right;
+    }
