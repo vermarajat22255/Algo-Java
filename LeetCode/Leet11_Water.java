5368428e17d6de3aa9a35f35738966f63591160b
vermarajat22255 <vermarajat22255@gmail.com>
2020-08-21 23:54:59 -0400

    add code

--- LeetCode/Leet11_Water.java
+++ LeetCode/Leet11_Water.java
@@ -37,3 +37,20 @@ class Solution {
         return (Math.abs(j-i)*Math.min(height[i],height[j]));
     }
 }
+// Try 2 without hint ;)
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i=0, j=height.length-1;
+        int water = 0;
+        while(i < j ){
+            water = Math.max(water, Math.min(height[i], height[j])*(j-i));
+            if(height[i] < height[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return water;
+    }
+}
--- LeetCode/Leet9_PalindromeNum.java
+++ LeetCode/Leet9_PalindromeNum.java
@@ -6,3 +6,20 @@ class Solution {
         return s.toString().equals(s.reverse().toString());
     }
 }
+
+//Solution 2
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        String s = x+"";
+        int i=0, j=s.length()-1;
+        while( i < j){
+            if(s.charAt(i) != s.charAt(j)) 
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
