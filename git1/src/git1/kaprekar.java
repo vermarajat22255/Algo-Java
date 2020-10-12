7d2a2fcccf39bc1374a9450231a880c32efa6b2f
Ankitkcse19 <67203958+Ankitkcse19@users.noreply.github.com>
2020-10-12 21:02:47 +0530

    kaprekar.java

--- git1/src/git1/kaprekar.java
+++ git1/src/git1/kaprekar.java
@@ -1,10 +1,36 @@
 package git1;
 
+import java.util.Scanner;
+
 public class kaprekar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		   Scanner ob=new Scanner(System.in);
+	    int t = ob.nextInt();
+int j;
+	    while(t!=0){
+	         //  while(true) {
+	            int n=ob.nextInt();
+	           
+	            int N=n*n;
+	            int tn=n;int c=0;	
+	            while(tn!=0)
+	            { 
+	                tn=tn/10;c++;
+	            }
+	            int q=(int)(N/Math.pow(10,c));
+	            int r=(int)(N%Math.pow(10,c));
+	             j=q+r; 
+	           
+	            if(j==n)
+	                System.out.println("1");
+	            else
+	                System.out.println("0");
+	     // }
+	    t--;
+	    }
+	    	}
 
-	}
+	
 
 }
